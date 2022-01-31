b14f1225da516773eb01832115dcd497ef639dc5
Raiyan178 <89090815+Raiyan178@users.noreply.github.com>
2022-01-31 18:05:13 +0600

    Update settings.py

--- fahadsproject/settings.py
+++ fahadsproject/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#drug^$gd+doo@4j^w^x)^p7biti4t1^1cwlnfdl#=n+*f)e)n'
+with open(os.path.join(BASE_DIR,'secre.txt)) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
